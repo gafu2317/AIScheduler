e9dc385ed9d3a2a59e609c25313711541caddc38
iceeyyu <jgwpdkm@gmail.com>
2024-09-15 16:16:52 +0900

    タスク分配の試行錯誤

--- test2.mjs
+++ test2.mjs
@@ -1,7 +1,31 @@
-import OpenAI from 'openai';
-import z from 'zod';
-import { zodFunction } from 'openai/helpers/zod';
+import OpenAI from "openai";
+import dotenv from "dotenv";
+import { date } from "zod";
 
-import dotenv from 'dotenv';
+dotenv.config(); //.envの内容を読み込む
 
-dotenv.config()
+const apiKey = process.env.CHATGPT_KEY;
+const client = new OpenAI({ apiKey: apiKey });
+
+const InputdataSchema ={
+   "type" : "object" ,
+   "properties" : {
+     taskdate : {
+        "type" : "date"
+     },
+     title : { 
+        "type" : "string" 
+     },
+     discription : {
+        "type" : "string"
+     },
+     taskdeadline : {
+        "type" : "date"
+     }
+     predictTaskTime : {
+        
+     }
+    } 
+
+}
+  

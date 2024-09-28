e47bb6e2d13e85c63896a8be60a413da0ab91c45
iceeyyu <jgwpdkm@gmail.com>
2024-09-28 16:41:11 +0900

    ポップアップ実装、エラーあり

---
+++ js/popup.js
@@ -0,0 +1,49 @@
+const clickBtn = document.getElementById('clickBtn');
+const popupWrapper = document.getElementById('popupWrapper');
+const close = document.getElementById('close');
+const Button = document.getElementById('Button');
+const taskColor = document.getElementById('taskColor');
+const colorPopupWrapper = document.getElementById('colorPopupWrapper');
+const colorClose = document.getElementById('colorClose');
+// ボタンをクリックしたときにポップアップを表示させる
+clickBtn.addEventListener('click', () => {
+  popupWrapper.style.display = "block";
+});
+
+taskColor.addEventListener('click',() => {
+  colorPopupWrapper.style.visibility = 'visible';
+  colorPopupWrapper.style.display = 'block';
+  
+});
+
+
+
+
+// ポップアップの外側又は「x」のマークをクリックしたときポップアップを閉じる
+popupWrapper.addEventListener('click', e => {
+  if (e.target.id === popupWrapper.id || e.target.id === close.id) {
+    popupWrapper.style.display = 'none';
+    
+  }
+});
+
+// colorPopupWrapper.addEventListener('click', e => {
+//   if (e.target.id === colorPopupWrapper.id || e.target.id === colorClose.id) {
+//     colorPopupWrapper.style.display = 'none';
+    
+//   }
+//  });
+
+colorPopupWrapper.addEventListener('click',function(e){
+  if(e.target.id === colorPopupWrapper.id  || e.target.id === colorClose.id){
+    colorPopupWrapper.style.visibility = 'hidden';
+  }
+});
+
+//入力された値を読み込む関数
+Button.addEventListener('click',function(){
+  const inputValue = document.getElementById('tasktitle').value;
+  alert(inputValue);
+});
+
+

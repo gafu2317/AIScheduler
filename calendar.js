6dfe3df8ea9a6269ecce9a9b447892f7ec21592c
Kaoru Kato <kaoru.kato.2003@icloud.com>
2024-10-06 23:24:43 +0900

    ログイン時処理

--- calendar.js
+++ calendar.js
@@ -1,7 +1,23 @@
 import { Calendar } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid"; // dayGrid プラグイン
 import timeGridPlugin from "@fullcalendar/timegrid"; // timeGrid プラグイン
+import { db, auth, provider } from "./fire.js";
+import { signInWithPopup, signOut } from "firebase/auth";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  updateDoc,
+  onSnapshot,
+  query,
+  where,
+  addDoc,
+  getFirestore,
 
+  // createCollectionAndAddDocument,
+} from "firebase/firestore";
 document.addEventListener("DOMContentLoaded", function () {
   var calendarMonthEl = document.getElementById("calendar-month");
   var calendarDayEl = document.getElementById("calendar-day");
@@ -34,4 +50,47 @@ document.addEventListener("DOMContentLoaded", function () {
 
   calendarMonth.render();
   calendarDay.render();
+
+  // ログイン処理
+  const loginButton = document.getElementById("loginButton");
+  loginButton.addEventListener("click", () => {
+    signInWithPopup(auth, provider)
+      .then(async (result) => {
+        if (result.user != null) {
+          const userId = result.user.uid;
+          console.log(result.user.uid);
+
+          // Firestoreからユーザーの予定を取得
+          const q = query(collection(db, userId));
+
+          const querySnapshot = await getDocs(q);
+          querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            const data = doc.data();
+
+            // イベントをカレンダーに追加
+            calendarMonth.addEvent({
+              id: doc.id,
+              title: data.title,
+              start: data.start.toDate(), // Firestore Timestamp -> JS Dateに変換
+              end: data.end.toDate(),
+              allDay: data.allDay,
+              backgroundColor: data.backgroundColor,
+            });
+            calendarDay.addEvent({
+              id: doc.id,
+              title: data.title,
+              start: data.start.toDate(), // Firestore Timestamp -> JS Dateに変換
+              end: data.end.toDate(),
+              allDay: data.allDay,
+              backgroundColor: data.backgroundColor,
+            });
+            console.log(doc.id, " => ", doc.data());
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Login error:", error);
+      });
+  });
 });

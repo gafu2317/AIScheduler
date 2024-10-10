7fcd536d3c0dac27b754a63abbea69a1cfae1d61
gafu2317 <0002317rynkmk@gmail.com>
2024-10-10 20:32:58 +0900

    コメント追加

--- js/popup.js
+++ js/popup.js
@@ -5,19 +5,19 @@ const Button = document.getElementById('Button');
 const taskColor = document.getElementById('taskColor');
 const colorPopupWrapper = document.getElementById('colorPopupWrapper');
 const colorClose = document.getElementById('colorClose');
+
+
 // ボタンをクリックしたときにポップアップを表示させる
 clickBtn.addEventListener('click', () => {
   popupWrapper.style.display = "block";
 });
 
+// タスクの色をクリックしたときにポップアップを表示させる
 taskColor.addEventListener('click',() => {
   colorPopupWrapper.style.visibility = 'visible';
   colorPopupWrapper.style.display = 'block';
   
 });
-
-
-
 
 // ポップアップの外側又は「x」のマークをクリックしたときポップアップを閉じる
 popupWrapper.addEventListener('click', e => {
@@ -27,20 +27,14 @@ popupWrapper.addEventListener('click', e => {
   }
 });
 
-// colorPopupWrapper.addEventListener('click', e => {
-//   if (e.target.id === colorPopupWrapper.id || e.target.id === colorClose.id) {
-//     colorPopupWrapper.style.display = 'none';
-    
-//   }
-//  });
-
+// タスクの色のポップアップの外側又は「x」のマークをクリックしたときポップアップを閉じる
 taskColor.addEventListener('click',function(e){
   if(e.target.id === colorPopupWrapper.id  || e.target.id === colorClose.id){
     colorPopupWrapper.style.visibility = 'hidden';
   }
 });
 
-//入力された値を読み込む関数
+//入力された値を読み込む関数(テスト用)
 Button.addEventListener('click',function(){
   const inputValue = document.getElementById('tasktitle').value;
   alert(inputValue);

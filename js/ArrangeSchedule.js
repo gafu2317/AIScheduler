05763fdcbd6aafc6dde3bca226f20eba5733723a
gafu2317 <0002317rynkmk@gmail.com>
2024-11-03 21:21:05 +0900

    timeUnit関数作成

--- js/ArrangeSchedule.js
+++ js/ArrangeSchedule.js
@@ -5,25 +5,28 @@ console.log(testResult);
 function displayData(data){
     ButtonPopupResult.innerHTML = setInnerHTML(data); 
 }
-function timeUnit(minutes){
-    
+function timeUnit(totalminutes){
+  const hours = Math.floor(totalminutes / 60);
+  const minutes = totalminutes % 60;
+  return hours + "時間" + minutes + "分";
 }
+
 function setInnerHTML(task){
     const data = task.tasks
   let HTMLcontent =`<div></div>`;
   for(let i=0;i<data.length;i++){
-    HTMLcontent+=`
-      <div class="title">タスク${(i+1)}回目</div>
+    HTMLcontent += `
+      <div class="title">タスク${i + 1}回目</div>
       <div class="dateContainer">
-        <div class="year">${data[i].year}</div>
+        <div class="year">${add(data[i].year)}</div>
         <div class="month">${data[i].month}</div>
         <div class="day">${data[i].day}</div>
       </div>
       <div class="timeContainer">
-        <div class="startTime">${data[i].}</div>
-        <div class="endTime">${data[i].}</div>
+        <div class="startTime">${timeUnit(data[i].StartMinutes)}</div>
+        <div class="endTime">${data[i].day}</div>
       </div>
-      `
+      `;
   }
   return HTMLcontent; 
 };
